6b6e43d0b4390a534b5ce6457de5521fe0d20b01
Alex Cipollone <alex.cipollone10@gmail.com>
2023-05-12 15:15:47 -0400

    3 tests remaining; code needs heavy refactoring

--- time_calculator.py
+++ time_calculator.py
@@ -16,49 +16,60 @@ def add_time(start, duration, day="None"):
   formatted_minutes = ""
   days_elapsed = ''
   get_number_of_days = ''
-  match_key = ''
-  next_day = ''
+  # day_key = ''
   new_time = ''
 
+  # STEP 1: Get the time of day
   get_TOD = start.split(" ")
   time_of_day = get_TOD[1]
-  print("AM or PM? ", time_of_day)
+  # print("AM or PM? ", time_of_day)
 
+  # STEP 2: Separate hours and minutes for your time
   regex = re.compile('[0-9:]+')
   filtered_string = regex.search(start)
   start_list = filtered_string.group(0).split(":")
   print("Start list: ", start_list)
-
+  
+  # STEP 3: Separate hours and minutes for your duration count
   filtered_string = regex.search(duration)
   duration_list = filtered_string.group(0).split(":")
   print("Duration list: ", duration_list)
 
+  #STEP 4: Define the starting hour; get the combined minutes
   start_hour = int(start_list[0])
-  print("Start hour is",start_hour)
   minutes = int(start_list[1]) + int(duration_list[1])
-  print("minutes = ",minutes)
 
+  def getDay():
+    day2 = day.lower()
+    day3 = day2.capitalize()
+    # For loop to match day with dict day
+    for i in range(1,len(days_of_week),1):
+        if (days_of_week[i] == day3):
+          global day_key
+          day_key = i
+  getDay()
+
+  
+  # STEP 5: Check if time is returning itself or 24 hours in advance
   if (duration_list[0] == '0' and duration_list[1] == '00'):
     formatted_minutes = str(minutes).zfill(2)
     new_time = str(start_hour) +":" + formatted_minutes + ' ' + time_of_day
     print(new_time)
     return new_time;
-
   if (duration_list[0] == '24' and duration_list[1] == '00'):
     formatted_minutes = str(minutes).zfill(2)
     new_time = str(start_hour) +":" + formatted_minutes + ' ' + time_of_day + " (next day)"
     print(new_time)
     return new_time;
 
+  # STEP 6: Convert PM to military time
   if (time_of_day == "PM"):
     start_hour = start_hour + 12
-  
   hour = start_hour + int(duration_list[0])
-    
-  
+
+  # STEP 7: Format minutes correctly and ensure they reset to zero on the hour
   if (minutes < 10):
     formatted_minutes = str(minutes).zfill(2)
-    
   elif (minutes > 59):
     if (duration_list[0] == '0'):
       hour = hour + 1
@@ -67,19 +78,21 @@ def add_time(start, duration, day="None"):
 
     else:
       minutes = minutes - 60
-      formatted_minutes = str(minutes).zfill(2)
-
-      
+      formatted_minutes = str(minutes).zfill(2)      
   else:
     formatted_minutes = str(minutes)
 
   if (hour >= 13 and time_of_day == "PM"):
     if(hour > 23):
       get_number_of_days = round(hour / 24)
-      # expected = "12:04 AM (2 days later)"
-      # print("Number of full days: ", get_number_of_days) # Here's the "20 days later"
-      # print("Extra hours = ",hour % 24)
-      # print("Hours entered: ",hour)
+      
+      if(day != "None"):
+        print("day key= ",day_key)
+        next_day = int(get_number_of_days) + day_key
+        if (next_day <= 7):
+          next_day = days_of_week[next_day]
+        else: 
+          next_day = days_of_week[next_day - get_number_of_days]
 
       if(get_number_of_days > 1):
         days_elapsed = " (" +str(get_number_of_days) + " days later)"
@@ -94,40 +107,36 @@ def add_time(start, duration, day="None"):
       
       formatted_hour = str(new_hour)
       time_of_day = "AM"
-      new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day + days_elapsed
-      
+
+      if(day != "None"):
+        new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day +", " + next_day + days_elapsed
+      else:
+        new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day + days_elapsed
+    # Converts AM to PM correctly
     else:
       hour = hour - 12
       formatted_hour = str(hour)
       time_of_day = "PM"
       new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day
 
+  # New time is ???
   elif (hour <= 12 and time_of_day == "AM"):
     formatted_hour = str(hour)
     time_of_day = "PM"
     new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day
 
+  # New time is between 1PM and 11:59PM
   elif (hour >= 13 and time_of_day == "AM"):
     formatted_hour = str(hour - 12)
     time_of_day = "PM"
     new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day
 
-  elif (hour < 13 and time_of_day == "PM"):
+  # elif (hour == 12 and time_of_day == "PM"):
+  # New time is 12PM - 12:59PM
+  else:
     formatted_hour = str(hour)
     new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day
     
-  day2 = day.lower()
-  day3 = day2.capitalize()
-
-  # For loop to match day with dict day
-  # for i in range(1,len(days_of_week),1):
-  #   for key,value in days_of_week.items():
-  #     match_key = key
-  #   if (days_of_week[i] == day3):
-  #     print("THE DAY IS: ",days_of_week[i])
-  #     next_day = int(get_number_of_days) + match_key
-  #     print("Next day = ", (round((next_day)/7)),"AKA ",days_of_week[round((next_day)/7)])
-
 
   print(new_time) 
   return new_time
